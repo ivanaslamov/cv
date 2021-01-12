5d4bd261ae31b3083e847b3604b506a4ae2827b3
Ivan Aslamov <ivan.aslamov@gmail.com>
2021-01-12 01:15:51 -0800

    code clean up

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import cv2
 
 from app.edges import eigen
-from app.utils import gaussian
+
 
 image = cv2.imread("data/beams.jpg")
+corners = eigen(image)
 
-window_size = 8
-ones_helper = gaussian(window_size)
-
-image = eigen(image)
-
-cv2.imshow('image', image)
+cv2.imshow('image', corners)
 cv2.waitKey()
 
 cv2.destroyAllWindows()

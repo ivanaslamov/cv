a1884151145392bc1a0e0640f1c928b3e6c72f5a
Ivan Aslamov <ivan.aslamov@gmail.com>
2021-02-08 19:51:10 -0800

    updates

--- main.py
+++ main.py
@@ -4,7 +4,28 @@ import numpy as np
 from scipy.optimize import line_search
 
 from app.edges import canny
+
 # from app.corners import shi, eigen
+
+# read image
+image = cv2.imread("data/checker_pattern.jpg")
+
+# select starting point
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # find all non zero points
@@ -12,17 +33,17 @@ from app.edges import canny
 points = []
 
 
-
 def obj_func(p):
     acc = 0
     for x, y in points:
-        acc += p[0] * x**2 + p[1] * x * y + p[2] * y**2 + p[3] * x + p[4] * y + p[5]
+        acc += p[0] * x ** 2 + p[1] * x * y + p[2] * y ** 2 + p[3] * x + p[4] * y + p[5]
     return acc
+
 
 def obj_grad(_):
     acc_a = 0
     for x, _ in points:
-        acc_a += x**2
+        acc_a += x ** 2
 
     acc_b = 0
     for x, y in points:
@@ -30,7 +51,7 @@ def obj_grad(_):
 
     acc_c = 0
     for _, y in points:
-        acc_c += y**2
+        acc_c += y ** 2
 
     acc_d = 0
     for x, _ in points:
@@ -47,25 +68,33 @@ def obj_grad(_):
     return [acc_a, acc_b, acc_c, acc_d, acc_e, acc_f]
 
 
-
 start_point = np.array([1.8, 1.7])
 search_gradient = np.array([-1.0, -1.0])
 
 results = line_search(obj_func, obj_grad, start_point, search_gradient)
 
+
+
+a = - sqrt(2 * (A*E**2 + C * D**2 - B*D*E+(B**2-4*A*C)*F)*( (A+C)+sqrt( (A-C)**2+B**2 ) )) / (B**2 - 4 * A * C)
+b = - sqrt(2 * (A*E**2 + C * D**2 - B*D*E+(B**2-4*A*C)*F)*( (A+C)-sqrt( (A-C)**2+B**2 ) )) / (B**2 - 4 * A * C)
+
+x0 = (2 * C * D - B * E) / (B**2 - 4 * A * C)
+y0 = (2 * A * E - B * D) / (B**2 - 4 * A * C)
+
+theta = arctan( 1 / B * (C - A - sqrt((A - C)**2 + B**2)) )
+
+
+color =
+thickness =
+image = cv2.ellipse(image, [x0,y0], [a,b], theta, 0, 360, color, thickness)
+
+
 print(results)
 
-
-
-
-
-
-image = cv2.imread("data/checker_pattern.jpg")
 corners = canny(image, 10, 5)
 
 cv2.imshow('image', corners)
 cv2.waitKey()
-
 
 # cap = cv2.VideoCapture(0)
 #

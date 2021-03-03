f6242e4a244d2378c088c737ad7629cfa852d044
Ivan Aslamov <ivan.aslamov@gmail.com>
2021-03-03 00:44:47 -0800

    copied snakes implementation

---
+++ libs/snakes/snakes_demo.py
@@ -0,0 +1,121 @@
+import sys
+import os
+import numpy as np
+from skimage import measure
+from functools import partial
+from scipy.interpolate import splprep, splev
+from scipy.integrate import simps
+from scipy.ndimage.filters import uniform_filter
+from scipy import ndimage
+from skimage import filters, feature, morphology
+import matplotlib
+import matplotlib.pyplot as plt
+import warnings
+from snakes import fit_snake
+
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
+
+
+def enhance_ridges(frame, mask=None):
+    """Detect ridges (larger hessian eigenvalue)"""
+    blurred = filters.gaussian(frame, 2)
+    hessians = feature.hessian_matrix(blurred, sigma=4.5, mode='nearest', order="xy")
+    ridges = feature.hessian_matrix_eigvals(hessians)[1]
+    return np.abs(ridges)
+
+
+
+def mask_to_boundary_pts(mask, pt_spacing=10):
+    """
+    Convert a binary image containing a single object to a set
+    of 2D points that are equally spaced along the object's contour.
+    """
+    # interpolate boundary
+    boundary_pts = measure.find_contours(mask, 0)[0]
+    tck, u = splprep(boundary_pts.T, u=None, s=0.0, per=1)
+    u_new = np.linspace(u.min(), u.max(), 1000)
+    x_new, y_new = splev(u_new, tck, der=0)
+
+    # get equi-spaced points along spline-interpolated boundary
+    x_diff, y_diff = np.diff(x_new), np.diff(y_new)
+    S = simps(np.sqrt(x_diff**2 + y_diff**2))
+    N = int(round(S/pt_spacing))
+
+    u_equidist = np.linspace(0, 1, N+1)
+    x_equidist, y_equidist = splev(u_equidist, tck, der=0)
+    return np.array(list(zip(x_equidist, y_equidist)))
+
+
+
+# load data: the raw image an a binary region-of-interest image
+im = np.load('cropped_frame.npy')
+mask = np.load('enlarged_mask.npy')
+# mask = np.load('shifted_mask.npy')
+# mask = np.load('shrunken_mask.npy')
+# mask = np.load('target_mask.npy')
+
+# get boundary points of mask
+boundary_pts = mask_to_boundary_pts(mask, pt_spacing=3)
+x, y = boundary_pts[:,1], boundary_pts[:,0]
+
+
+# distance from ridge midlines
+ridges = enhance_ridges(im)
+thresh = filters.threshold_otsu(ridges)
+prominent_ridges = ridges > 0.8*thresh
+skeleton = morphology.skeletonize(prominent_ridges)
+edge_dist = ndimage.distance_transform_edt(~skeleton)
+edge_dist = filters.gaussian(edge_dist, sigma=2)
+
+
+# distance from skeleton branch points
+blurred_skeleton = uniform_filter(skeleton.astype(float), size=3)
+corner_im = blurred_skeleton > 4./9
+corners_labels = measure.label(corner_im)
+corners = np.array([region.centroid for region in measure.regionprops(corners_labels)])
+
+
+
+# show the intermediate images
+plt.gray()
+plt.ion()
+plt.subplot(221)
+plt.imshow(im)
+plt.title('original image')
+plt.axis('off')
+plt.subplot(222)
+plt.imshow(ridges)
+plt.title('ridge filter')
+plt.axis('off')
+plt.subplot(223)
+plt.imshow(skeleton)
+plt.plot(corners[:,1], corners[:,0], 'ro')
+plt.title('ridge skeleton w/ branch points')
+plt.axis('off')
+plt.subplot(223)
+plt.imshow(skeleton)
+plt.autoscale(False)
+plt.plot(corners[:,1], corners[:,0], 'ro')
+plt.title('ridge skeleton w/ branch points')
+plt.subplot(224)
+plt.imshow(edge_dist)
+plt.title('distance transform of skeleton')
+plt.axis('off')
+plt.ioff()
+plt.show()
+
+
+# show an animation of the fitting procedure
+fig = plt.figure()
+plt.imshow(im, cmap='gray')
+plt.plot(x, y, 'bo')
+line_obj, = plt.plot(x, y, 'ro')
+plt.axis('off')
+    
+plt.ion()
+plt.pause(0.01)
+snake_pts = fit_snake(boundary_pts, edge_dist, nits=60, alpha=0.5, beta=0.2, point_plot=line_obj)
+plt.ioff()
+plt.pause(0.01)
+plt.show()
+    
--- main.py
+++ main.py
@@ -4,109 +4,142 @@ from scipy.optimize import minimize
 
 from app.utils import gradient_magnitude, distance, curvature
 
-image = cv2.imread("data/dark_circle.png")
-
-if len(image.shape) == 3:
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-elif len(image.shape) == 4:
-    gray = cv2.cvtColor(image, cv2.COLOR_RGBA2GRAY)
-else:
-    gray = image.clone()
-
-height, width = gray.shape
-
-
-# start points
-start_points = [
-    (250, 150),
-    (250, 200),
-    (250, 250),
-    (250, 300),
-    (250, 350),
-]
-
-start_points_length = len(start_points)
-
-flatten_start_points = []
-for x, y in start_points:
-    flatten_start_points.append(x)
-    flatten_start_points.append(y)
-
-start_point = np.array(flatten_start_points)
-
-# gradient magnitude
-mag = 255 - gradient_magnitude(gray, kernel_size=32)*100
-
-color_mag = cv2.cvtColor(mag, cv2.COLOR_GRAY2RGB)
-
-def obj_func(p):
-    # draw
-    global color_mag
-
-    temp = color_mag.copy()
-
-    for i in range(0, len(p), 2):
-        x = int(p[i])
-        y = int(p[i + 1])
-        temp = cv2.circle(temp, (x, y), 5, (255, 0, 0), 5)
-
-    # display results
-    cv2.imshow('frame', temp)
-    cv2.waitKey(10)
-
-    # continuity
-    sum_distance = 0
-
-    for i in range(0, len(p)-2, 2):
-        sum_distance += distance(p[i], p[i+1], p[i+2], p[i+3])
-
-    mean_distance = 2 * sum_distance / len(p)
-
-    continuity_acc = (mean_distance - sum_distance)**2
-
-    # curvature
-    curvature_acc = 0
-
-    for i in range(2, len(p)-2, 2):
-        curvature_acc += curvature(p[i-2], p[i-1], p[i], p[i+1], p[i+2], p[i+3])
-
-    # magintude
-    mag_acc = 0
-
-    for i in range(0, len(p), 2):
-        x0 = int(p[i])
-        y0 = int(p[i+1])
-
-        value = mag[y0, x0]
-
-        print(value)
-
-        mag_acc += value
-
-    print(mag_acc)
-
-    # return value0 + value1 + value2 + abs(10 - dist0)*0.1 + abs(10 - dist1)*0.1
-    return mag_acc # continuity_acc + curvature_acc + mag_acc
-
-res = minimize(obj_func, start_point, options={'eps': 3})
-print(res.x)
-
-print(start_points)
-
-# print results
+# image = cv2.imread("data/dark_circle.png")
+#
+# if len(image.shape) == 3:
+#     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# elif len(image.shape) == 4:
+#     gray = cv2.cvtColor(image, cv2.COLOR_RGBA2GRAY)
+# else:
+#     gray = image.clone()
+#
+# height, width = gray.shape
+#
+#
+# # start points
+# start_points = [
+#     (250, 150),
+#     (250, 200),
+#     (250, 250),
+#     (250, 300),
+#     (250, 350),
+# ]
+#
+# start_points_length = len(start_points)
+#
+# flatten_start_points = []
 # for x, y in start_points:
-#     color_mag = cv2.circle(color_mag, (x, y), 5, (255, 255, 0), 5)
+#     flatten_start_points.append(x)
+#     flatten_start_points.append(y)
+#
+# start_point = np.array(flatten_start_points)
+#
+# # gradient magnitude
+# mag = 255 - gradient_magnitude(gray, kernel_size=32)*100
+#
+# color_mag = cv2.cvtColor(mag, cv2.COLOR_GRAY2RGB)
+#
+# def obj_func(p):
+#     # draw
+#     global color_mag
+#
+#     temp = color_mag.copy()
+#
+#     for i in range(0, len(p), 2):
+#         x = int(p[i])
+#         y = int(p[i + 1])
+#         temp = cv2.circle(temp, (x, y), 5, (255, 0, 0), 5)
+#
+#     # display results
+#     cv2.imshow('frame', temp)
+#     cv2.waitKey(10)
+#
+#     # continuity
+#     sum_distance = 0
+#
+#     for i in range(0, len(p)-2, 2):
+#         sum_distance += distance(p[i], p[i+1], p[i+2], p[i+3])
+#
+#     mean_distance = 2 * sum_distance / len(p)
+#
+#     continuity_acc = (mean_distance - sum_distance)**2
+#
+#     # curvature
+#     curvature_acc = 0
+#
+#     for i in range(2, len(p)-2, 2):
+#         curvature_acc += curvature(p[i-2], p[i-1], p[i], p[i+1], p[i+2], p[i+3])
+#
+#     # magintude
+#     mag_acc = 0
+#
+#     for i in range(0, len(p), 2):
+#         x0 = int(p[i])
+#         y0 = int(p[i+1])
+#
+#         value = mag[y0, x0]
+#
+#         print(value)
+#
+#         mag_acc += value
+#
+#     print(mag_acc)
+#
+#     # return value0 + value1 + value2 + abs(10 - dist0)*0.1 + abs(10 - dist1)*0.1
+#     return mag_acc # continuity_acc + curvature_acc + mag_acc
+#
+# res = minimize(obj_func, start_point, options={'eps': 3})
+# print(res.x)
+#
+# print(start_points)
+#
+# # print results
+# # for x, y in start_points:
+# #     color_mag = cv2.circle(color_mag, (x, y), 5, (255, 255, 0), 5)
+#
+# color_mag = color_mag.copy()
+#
+# for i in range(0, len(res.x), 2):
+#     x = int(res.x[i])
+#     y = int(res.x[i+1])
+#     color_mag = cv2.circle(color_mag, (x, y), 5, (255, 0, 0), 5)
+#
+# # display results
+# cv2.imshow('frame', color_mag)
+# cv2.waitKey(10000)
+#
+# # clean results
+# cv2.destroyAllWindows()
 
-color_mag = color_mag.copy()
+import numpy as np
 
-for i in range(0, len(res.x), 2):
-    x = int(res.x[i])
-    y = int(res.x[i+1])
-    color_mag = cv2.circle(color_mag, (x, y), 5, (255, 0, 0), 5)
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+from skimage import data
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
 
-# display results
-cv2.imshow('frame', color_mag)
-cv2.waitKey(10000)
 
-# clean results
-cv2.destroyAllWindows()
+img = cv2.imread("data/bird.jpg")
+
+if len(img.shape) == 3:
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+elif len(img.shape) == 4:
+    img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+
+s = np.linspace(0, 2*np.pi, 400)
+r = 200 + 180*np.sin(s)
+c = 320 + 270*np.cos(s)
+init = np.array([r, c]).T
+
+snake = active_contour(gaussian(img, 3),
+                       init, alpha=0.015, beta=10, gamma=0.001)
+
+fig, ax = plt.subplots(figsize=(7, 7))
+ax.imshow(img, cmap=plt.cm.gray)
+ax.plot(init[:, 1], init[:, 0], '--r', lw=3)
+ax.plot(snake[:, 1], snake[:, 0], '-b', lw=3)
+ax.set_xticks([]), ax.set_yticks([])
+ax.axis([0, img.shape[1], img.shape[0], 0])
+
+plt.show()
